ffca470759133c4051f82604b0b9f86d2b9b365a
Baker <bkralhsn232@gmail.com>
2026-05-20 23:43:01 +0300

    e

---
+++ src/models/comments.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema({
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Movie',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, { timestamps: true });
+
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
