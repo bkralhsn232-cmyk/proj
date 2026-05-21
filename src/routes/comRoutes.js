b69c2676572bc8760061212ab111f3f49bcda01d
Baker <bkralhsn232@gmail.com>
2026-05-21 03:56:38 +0300

    e

--- src/routes/comRoutes.js
+++ src/routes/comRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Comment from '../models/comments.js';
-import User from '../models/User.js'; 
+import User from '../models/user.js'; 
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
